3e5bc229104c451762c643e7b7c03ebca701fd80
jhanvisunildesai <jhanvisunildesai@gmail.com>
2026-07-18 23:01:35 +0530

    Add: contains-duplicate-ii - submission-10

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-10.py
@@ -0,0 +1,30 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        retval = False
+        i = 0
+        temp = {}
+        for j in range(i, min(len(nums),k+1)):
+            if nums[j] in temp:
+                temp[nums[j]] += 1
+            else:
+                temp[nums[j]] = 1
+        print(temp)
+        if any(value > 1 for value in temp.values()):
+            retval = True
+        else:
+            for j in range(k+1, len(nums)):
+                # print(temp)
+                if temp[nums[i]] == 1:
+                    del temp[nums[i]]
+                else:
+                    temp[nums[i]] -= 1
+                i += 1
+                if nums[j] in temp:
+                    temp[nums[j]] += 1
+                else:
+                    temp[nums[j]] = 1
+                if any(value > 1 for value in temp.values()):
+                    retval = True
+                    break
+        return retval
+
